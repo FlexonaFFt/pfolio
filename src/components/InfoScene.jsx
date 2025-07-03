af85fc70cda486715dbf6334264a3d927fe08e36
FlexonaFFt <flexonafft@bk.ru>
2025-07-03 23:44:47 +0300

    --init info section

--- src/components/InfoScene.jsx
+++ src/components/InfoScene.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import '../assets/InfoScene.css';
 
 export default function InfoScene() {
   return (
-    <div className="info-scene">
-      <h1>Info Scene</h1>
-      <p>This is the information section.</p>
+    <div className='info-scene'>
+        <div className='left-block'>
+            <div className='text-section'>
+                <p>Find Inspiration.<br/>Create, Generate,<br/>Produce&Automate</p>
+                <p className="bottom-text">Welcome to PromptVerse. Effortlessly create content, explore endless prompts, 
+                    and stay ahead with real-time trends. Automate emails, social media, and more 
+                    while our AI extracts knowledge from any document or URL. Experience a stunning, 
+                    futuristic design that boosts productivity.</p>
+                <p className="bottom-text">Welcome to PromptVerse. Effortlessly create content, explore endless prompts, 
+                    and stay ahead with real-time trends. Automate emails, social media, and more 
+                    while our AI extracts knowledge from any document or URL. Experience a stunning, 
+                    futuristic design that boosts productivity.</p>
+                <p className='link-text'>Show more about me →</p>
+            </div>
+        </div>
+        <div className='right-block'>
+
+        </div>
     </div>
   );
 }
