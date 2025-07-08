0ae6ed41f7f8477446a053b9fd1191e9367855ff
FlexonaFFt <flexonafft@bk.ru>
2025-07-08 12:29:35 +0300

    --update sections

--- src/components/StackScene.jsx
+++ src/components/StackScene.jsx
@@ -9,7 +9,7 @@ export default function StackScene() {
                 <div className='content-section design-section'>
                     <div className='left-text'>
                         <ShinyText text="01" disabled={false} speed={3} className='pointer' />
-                        <ShinyText text="Design" disabled={false} speed={3} className='point-text' />
+                        <ShinyText text="My Projects" disabled={false} speed={3} className='point-text' />
                     </div>
                     <div className='right-text'>
                         <ScrambleText className="text-line" speed={20} useOriginalCharsOnly={true} sequential={true} text="We deeply care about our work."></ScrambleText>
@@ -17,6 +17,7 @@ export default function StackScene() {
                         <ScrambleText className="text-line" speed={20} useOriginalCharsOnly={true} sequential={true} text="each project reflects our commitment"></ScrambleText>
                         <ScrambleText className="text-line" speed={20} useOriginalCharsOnly={true} sequential={true} text="to excellence and clinet success,"></ScrambleText>
                         <ScrambleText className="text-line" speed={20} useOriginalCharsOnly={true} sequential={true} text="a source of immense preid for us."></ScrambleText>
+                        <ShinyText text="Show more about this section" disabled={false} speed={3} className='linked-text' />
                     </div>
                 </div>
                 
@@ -30,19 +31,21 @@ export default function StackScene() {
                         <ScrambleText className="text-line" speed={20} useOriginalCharsOnly={true} sequential={true} text="harnesses leading-edge technology"></ScrambleText>
                         <ScrambleText className="text-line" speed={20} useOriginalCharsOnly={true} sequential={true} text="to create products designed to"></ScrambleText>
                         <ScrambleText className="text-line" speed={20} useOriginalCharsOnly={true} sequential={true} text="achive your goals."></ScrambleText>
+                        <ShinyText text="Show more about this section" disabled={false} speed={3} className='linked-text' />
                     </div>
                 </div>
                 
                 <div className='content-section synergy-section'>
                     <div className='left-text'>
                         <ShinyText text="03" disabled={false} speed={3} className='pointer' />
-                        <ShinyText text="Synergy" disabled={false} speed={3} className='point-text' />
+                        <ShinyText text="Motivation" disabled={false} speed={3} className='point-text' />
                     </div>
                     <div className='right-text'>
                         <ScrambleText className="text-line" speed={20} useOriginalCharsOnly={true} sequential={true} text="Our code - both literally and figuratively -"></ScrambleText>
                         <ScrambleText className="text-line" speed={20} useOriginalCharsOnly={true} sequential={true} text="is collaboration. Fine-tuning each small"></ScrambleText>
                         <ScrambleText className="text-line" speed={20} useOriginalCharsOnly={true} sequential={true} text="detail until we can take a step back"></ScrambleText>
                         <ScrambleText className="text-line" speed={20} useOriginalCharsOnly={true} sequential={true} text="and marvel at the whole picture, together."></ScrambleText>
+                        <ShinyText text="Show more about this section" disabled={false} speed={3} className='linked-text' />
                     </div>
                 </div>
             </div>
