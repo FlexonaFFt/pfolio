c23ad2fd91efd13406cb215d16ba55b3f873f5c7
FlexonaFFt <flexonafft@bk.ru>
2025-07-11 19:58:17 +0300

    --init reform

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './main.css';
 
 import IntroScene from './components/IntroScene.jsx';
-import InfoScene from './components/InfoScene.jsx';
-import StackScene from './components/StackScene.jsx';
-import AboutMe from './components/AboutMe.jsx';
 
 const Portfolio = () => {
   return (
-    <BrowserRouter>
-      <div className='portfolio'>
-        <Routes>
-          <Route path="/" element={
-            <>
-              <section className="scroll-section">
-                <IntroScene />
-              </section>
-              <section className="scroll-section">
-                <InfoScene />
-              </section>
-              <section className="scroll-section stack-scene-container">
-                <StackScene />
-              </section>
-            </>
-          } />
-          <Route path="/aboutme" element={<AboutMe />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className='portfolio'>
+      <section className="scroll-section">
+        <IntroScene />
+      </section>
+    </div>
   );
 }
 
@@ -37,4 +18,4 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Portfolio />
   </StrictMode>,
-)
+);
